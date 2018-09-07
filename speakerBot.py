d54e271ba3f479aab3cea5f5c45b75f93b6c9ce7
Deiner Zapata <deinerzapata@gmail.com>
2018-09-07 18:12:39 -0500

    Adding code

---
+++ speakerBot.py
@@ -0,0 +1,26 @@
+#coding:utf-8
+# pip install pyttsx3
+
+import os
+import sys
+import pyttsx3
+
+engine = pyttsx3.init()
+engine.setProperty('volume',1.0)
+engine.setProperty('rate',230)
+engine.setProperty('voice','spanish')
+
+def speak(texto):
+    engine.say(texto)
+    engine.runAndWait()
+
+def readFileTXT(nameFileTXT):
+    fullText = open(nameFileTXT)
+    speak("Leyendo archivo ["+nameFileTXT+"]")
+    for linea in fullText.readlines():
+        linea = linea[:-1]
+        speak(linea)
+
+
+speak("hola mundo, probando sintetizador de voz")
+readFileTXT("text2read.txt")
